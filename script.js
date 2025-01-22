25564dac46a708e344d70395bfa24c2f6ed87b59
nocodenorbert <115705191+Spaceminddog@users.noreply.github.com>
2025-01-22 16:06:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Optional: Add interactivity or animations here
-document.querySelector('.cta').addEventListener('click', () => {
-    alert('Thanks for your interest! Scroll down to learn more.');
-});
