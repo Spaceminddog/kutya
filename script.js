81cf7ec8d4c0626a1b092a9112a88638bf94a4c4
nocodenorbert <115705191+Spaceminddog@users.noreply.github.com>
2025-01-22 10:31:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// Optional: Add interactivity or animations here
+document.querySelector('.cta').addEventListener('click', () => {
+    alert('Thanks for your interest! Scroll down to learn more.');
+});
 
